9ec74b7441243c0a917913bb299842180d1e1690
Tomer Verona <tomerv@pansieve.com>
2013-07-02 10:57:48 -0700

    derive from activity rather than support.v4.app.fragmentactivity

--- library/src/com/jeremyfeinstein/slidingmenu/lib/app/SlidingFragmentActivity.java
+++ library/src/com/jeremyfeinstein/slidingmenu/lib/app/SlidingFragmentActivity.java
@@ -1,14 +1,14 @@
 package com.jeremyfeinstein.slidingmenu.lib.app;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.app.Activity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
-public class SlidingFragmentActivity extends FragmentActivity implements SlidingActivityBase {
+public class SlidingFragmentActivity extends Activity implements SlidingActivityBase {
 
 	private SlidingActivityHelper mHelper;
 
